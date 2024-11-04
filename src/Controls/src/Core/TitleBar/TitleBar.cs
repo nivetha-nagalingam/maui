b436625aef49635d368077bc56bf156d2b732a70
tj-devel709 <tj.devel709@gmail.com>
2024-11-04 09:35:36 -0600

    last noise, hopefully

--- src/Controls/src/Core/TitleBar/TitleBar.cs
+++ src/Controls/src/Core/TitleBar/TitleBar.cs
@@ -411,7 +411,7 @@ namespace Microsoft.Maui.Controls
 				HeightRequest = 16,
 				VerticalOptions = LayoutOptions.Center,
 				Margin = new Thickness(16, 0, 0, 0),
-				IsVisible = false
+				IsVisible = false,
 			};
 
 			contentGrid.Add(icon);
